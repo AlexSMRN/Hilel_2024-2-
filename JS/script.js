2a9f613356cccdf00e9cd4506fb1d6ba41178192
AlexSMRN <alexsahka0712@gmail.com>
2024-03-26 12:13:46 +0200

    update teamplate

---
+++ JS/script.js
@@ -0,0 +1,55 @@
+const sorted = movies.sort((a,b) => b.year - a.year)
+
+
+const favourites = sorted.slice(0,16)
+
+
+const cardTitles = document.querySelectorAll(".title p")
+favourites.forEach((movies, index) => {
+    if (index < cardTitles.length) {
+        cardTitles[index].textContent = movies.title
+    }
+});
+
+const cardDescriptions = document.querySelectorAll(".description p")
+favourites.forEach((movies, index) => {
+    if (index < cardDescriptions.length) {
+        cardDescriptions[index].textContent = movies.extract
+    }
+})
+
+const cardImages = document.querySelectorAll(".card img")
+favourites.forEach((movies, index) => {
+    if (movies.thumbnail) {
+        const img = new Image ()
+        img.onload = function () {
+            cardImages[index].src = movies.thumbnail
+        }
+        img.onerror = function() {
+            cardImages[index].src = "images/img1.jpg"
+        }
+        img.src = movies.thumbnail
+    } else {
+        cardImages[index].src = "images/img1.jpg"
+    }
+
+    cardImages[index].alt = movies.title
+
+});
+
+const filtered = movies.filter(function(movie) {
+    return movie.genres.includes("Action")
+})
+
+console.log("Filtered: ",filtered);
+
+const genres = movies.reduce(function (acc, movie) {
+    movie.genres.forEach(function(genre) {
+        if (!acc.includes(genre)) {
+            acc.push(genre)
+        }
+    });
+    return acc
+}, [])
+
+console.log("Genres: ",genres);
