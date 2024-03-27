c6b49a1610835b6772c875a39ab177047dbd0f5c
AlexSMRN <alexsahka0712@gmail.com>
2024-03-27 12:33:19 +0200

    update teamplate

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+$(".nav-link").click(function(e) {
+    const eventTarget =  e.target;
+    const targetSelector = $(eventTarget).data('target')
+
+    $('.nav-link').removeClass('active')
+    $(eventTarget).addClass('active');
+
+    $('.tab-pane').removeClass("active");
+    $('.tab-pane').addClass("fade");
+    $(targetSelector).addClass("active");
+    $(targetSelector).removeClass("fade");
+    
+})
