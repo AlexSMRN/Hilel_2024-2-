7a839e3c30988ad1b1b3812aba784accab20fc48
AlexSMRN <alexsahka0712@gmail.com>
2024-04-09 18:52:30 +0300

    update teamplate

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,130 @@
-const sorted = movies.sort((a,b) => b.year - a.year)
+console.log('It works!')
 
+class User {
+    _forename;
+    _surname;
+    _year;
 
-const favourites = sorted.slice(0,16)
+    constructor(forename, surname, year) {
+        this._forename = forename
+        this._surname = surname
+        this._year = year
+    };
 
+    get forename() {
+        return this._forename
+    };
 
-const cardTitles = document.querySelectorAll(".title p")
-favourites.forEach((movies, index) => {
-    if (index < cardTitles.length) {
-        cardTitles[index].textContent = movies.title
-    }
-});
+    get surname() {
+        return this._surname
+    };
 
-const cardDescriptions = document.querySelectorAll(".description p")
-favourites.forEach((movies, index) => {
-    if (index < cardDescriptions.length) {
-        cardDescriptions[index].textContent = movies.extract
-    }
-})
+    get year() {
+        return this._year
+    };
 
-const cardImages = document.querySelectorAll(".card img")
-favourites.forEach((movies, index) => {
-    if (movies.thumbnail) {
-        const img = new Image ()
-        img.onload = function () {
-            cardImages[index].src = movies.thumbnail
+    get age() {
+        return (new Date).getFullYear() - this._year
+    };
+
+    get fullName() {
+        return this._forename + " " + this._surname
+    };
+};
+
+class Student extends User {
+    _mark;
+    _presence;
+
+    constructor(forename, surname, year) {
+        super(forename, surname, year);
+        this._mark = new Array(30);
+        this._presence = new Array(30).fill(null);
+    };
+
+    present() {
+        const index = this._presence.findIndex(value => value === null)
+
+        if(index !== -1){
+            this._presence[index] = true;
+            return true 
+        };
+
+        return false 
+    };
+
+    absent() {
+        const index = this._presence.findIndex(value => value === null)
+
+        if(index !== -1){
+            this._presence[index] = false;
+            return false
+        };
+    };
+
+    setMark(mark) {
+        if (typeof mark === "number" && mark >= 0 && mark <= 10) {
+            const index = this._mark.findIndex(value => typeof value !== "number");
+            if (index !== -1) {
+                this._mark[index] = mark;
+                return mark;
+            } else {
+                console.log("Mark" + mark + " is invalid. It should be between 0 and 10");
+            }
         }
-        img.onerror = function() {
-            cardImages[index].src = "images/img1.jpg"
-        }
-        img.src = movies.thumbnail
-    } else {
-        cardImages[index].src = "images/img1.jpg"
+    };
+
+    mediumMark() {
+        let marks = this._mark.filter(mark => typeof mark === "number");
+        let sum = marks.reduce((acc, el) => acc + el, 0);
+        let medium = Math.round(sum / marks.length)
+
+        return medium
+    };
+
+    mediumVisit() {
+        let visits = this._presence.filter(presence => presence === true).length;
+        let totalVisits = this._presence.length;
+        let medium = Math.round((visits / totalVisits) * 100) / 10
+        
+        return medium
+    }  ;
+    
+    summary () {
+        if(this.mediumMark() >= 9 && this.mediumVisit() >= 0.9){
+            console.log("Medium mark: " + this.mediumMark() + " Medium visit: " + this.mediumVisit() + " Well done!")
+        } else if (this.mediumMark() >= 9 || this.mediumMark() < 9 && this.mediumVisit() >= 0.9 || this.mediumVisit() <= 0.9 ){
+            console.log("Medium mark: " + this.mediumMark() + " Medium visit: " + this.mediumVisit() + " Good, but you could do better")
+        } else {
+            console.log("Medium mark: " + this.mediumMark() + " Medium visit: " + this.mediumVisit() + " Bad, you will learn nothing this way")
+        };
+    };
+};
+
+class Teacher extends Student {
+    _groups;
+
+    constructor(forename, surname, year) {
+        super(forename, surname, year);
+        this._groups = [];
     }
 
-    cardImages[index].alt = movies.title
+    get groups() {
+        return this._groups;
+    }
 
-});
+    addGroups(group) {
+        this._groups.push(group);
+    }
 
-const filtered = movies.filter(function(movie) {
-    return movie.genres.includes("Action")
-})
-
-console.log("Filtered: ",filtered);
-
-const genres = movies.reduce(function (acc, movie) {
-    movie.genres.forEach(function(genre) {
-        if (!acc.includes(genre)) {
-            acc.push(genre)
+    changeStatus(group) {
+        const index = this._groups.findIndex(value => value.name === group.name);
+        if (index !== -1) {
+            this._groups[index].active = !this._groups[index].active;
         }
-    });
-    return acc
-}, [])
+    }
 
-console.log("Genres: ",genres);
+    get activeGroups() {
+        return this._groups.filter(group => group.active);
+    }
+};
