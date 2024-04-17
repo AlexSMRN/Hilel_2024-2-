4a2da33b37ff1de07ea1a30abdf240dd13255f6e
AlexSMRN <alexsahka0712@gmail.com>
2024-04-17 20:20:08 +0300

    update teamplate

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,37 @@
-console.log('It works!');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector(".navigation");
+const ul = document.querySelector(".navigation ul");
+const li = document.querySelectorAll("ul li");
+const main = document.querySelector("main")
+const footer = document.querySelector("footer")
+let clickCounter = 0;
 
-const userNameTarget = document.getElementById('userName');
+burger.addEventListener('click', () => {
+    clickCounter++;
+    burger.classList.add("burger-active");
+    burger.classList.add("new");
+    ul.classList.add("flex-active");
+    ul.classList.add("menu");
+    nav.classList.add("block");
+    main.classList.add("none");
+    footer.classList.add("none");
 
-function formHandler(event) {
-    event.preventDefault();
+    li.forEach(item => {
+        item.classList.add("menu-li");
+    });
+    
+    if (clickCounter === 2) {
+        clickCounter = 0;
+        burger.classList.remove("burger-active");
+        burger.classList.remove("new");
+        ul.classList.remove("flex-active");
+        ul.classList.remove("menu");
+        nav.classList.remove("block");
+        main.classList.remove("none");
+        footer.classList.remove("none");
 
-}
-
-function logoutHandler(event) {
-}
-
-function toggleSections(hasUser = false) {
-}
-
-function getFromStorage(name) {
-}
+        li.forEach(item => {
+            item.classList.remove("menu-li");
+        });
+    }
+});
